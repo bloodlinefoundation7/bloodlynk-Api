fedf0b3160c16908d83b53c9bd86f773b0e586b6
Syed Nazeer <nazeersyed117@gmail.com>
2026-01-01 18:08:20 +0530

    Initial api setup
    Initial api setup

---
+++ src/main/java/com/bloodlynk/bloodlynk_api/BloodlynkApiApplication.java
@@ -0,0 +1,13 @@
+package com.bloodlynk.bloodlynk_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodlynkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodlynkApiApplication.class, args);
+	}
+
+}
